3bd059bcde20d6288e874901dbe19f18626ac5d1
Tiago Mantello <41765111+tmantello@users.noreply.github.com>
2018-10-10 17:09:28 -0300

    última versão
    com sub rotas de usando id

--- src/app.js
+++ src/app.js
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import App from "./pages/App";
-
 import Home from "./pages/Home";
 import Cursos from "./pages/Cursos";
 import Sobre from "./pages/Sobre";
+import Curso from "./components/Curso";
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="/cursos" component={Cursos} />
-      <Route path="/sobre" component={Sobre} />
+      <Route path="cursos" component={Cursos}>
+        <Route path=":id" component={Curso} />
+      </Route>
+      <Route path="sobre" component={Sobre} />
     </Route>
   </Router>,
   document.getElementById("app")
