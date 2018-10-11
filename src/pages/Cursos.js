1464fae65338e3015eefab8db5efcb2664adf4bc
Tiago Mantello <41765111+tmantello@users.noreply.github.com>
2018-10-11 11:57:54 -0300

    nova versão
    com foreach(map) de cursos de um arquivo json

--- src/pages/Cursos.js
+++ src/pages/Cursos.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import messages from "../data/cursos.json";
+
 export default class Cursos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: messages
+    };
+  }
+
   render() {
-    return <h1 className="title">Você está na página de cursos!</h1>;
+    return (
+      <div>
+        <h1 className="title">Você está na página de cursos!</h1>
+        {this.props.children}
+      </div>
+    );
   }
 }
